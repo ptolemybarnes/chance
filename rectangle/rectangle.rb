78ea61fefa923bc02bfc21e5526503167d2548d4
Fred George <fredgeorge@acm.org>
2016-02-20 13:51:41 +0000

    Fred: Completed Rectangle class

--- rectangle/rectangle.rb
+++ rectangle/rectangle.rb
@@ -4,11 +4,16 @@
 class Rectangle
 
   def initialize(length, width)
+    raise "Dimension cannot be negative" unless length >= 0 && width >= 0 
     @length, @width = length, width
   end
   
   def area
     @length * @width
+  end
+
+  def perimeter
+    2 * (@length + @width)
   end
   
 end
--- rectangle/rectangle_test.rb
+++ rectangle/rectangle_test.rb
@@ -10,4 +10,12 @@ class RectangleTest < Minitest::Test
     assert_equal(24, Rectangle.new(4, 6).area)
   end
 
+  def test_perimeter
+    assert_equal(20, Rectangle.new(4, 6).perimeter)
+  end
+
+  def test_invalid_rectangle
+    assert_raises(Exception) { Rectangle.new(-1, 2) }
+  end
+
 end
